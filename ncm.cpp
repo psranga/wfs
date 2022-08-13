94c5caf993ea9b2b85c334600e295f4c1d96f79f
Ranga Sankaralingam <ranga@utexas.edu>
2022-08-12 17:15:51 -0700

    ncm.cpp cleanups.

--- ncm.cpp
+++ ncm.cpp
@@ -14,31 +14,9 @@ extern "C" {
 #include <lualib.h>
 }
 
-static void print_space_info(auto const& dirs, int width = 14)
-{
-    std::cout << std::left;
-    for (const auto s : {"Capacity", "Free", "Available", "Dir"})
-        std::cout << "│ " << std::setw(width) << s << ' ';
-    std::cout << '\n';
-    std::error_code ec;
-    for (auto const& dir : dirs) {
-        const std::filesystem::space_info si = std::filesystem::space(dir, ec);
-        std::cout
-            << "│ " << std::setw(width) << static_cast<std::intmax_t>(si.capacity) << ' '
-            << "│ " << std::setw(width) << static_cast<std::intmax_t>(si.free) << ' '
-            << "│ " << std::setw(width) << static_cast<std::intmax_t>(si.available) << ' '
-            << "│ " << dir << '\n';
-    }
-}
- 
-static int ncm_add1(lua_State* L) {
-  int n = (int)lua_tonumber(L, 1);
-  n = n + 1;
-  lua_pushnumber(L, n);
-  return 1;
-}
+namespace wfsint {
 
-static std::string int_to_hex64(std::uint64_t n) {
+std::string int_to_hex64(std::uint64_t n) {
 #if 1
   std::ostringstream os;
   os << "0x" << std::setbase(16) << std::setw(16) << std::setfill('0') << n << std::flush;
@@ -58,9 +36,13 @@ static std::string int_to_hex64(std::uint64_t n) {
 #endif
 }
 
-// -------------------------------------------
+} // namespace wfsint
 
-static int ncm_fs_space(lua_State* L) {
+namespace wfslua {
+
+using namespace wfsint;
+
+int ncm_fs_space(lua_State* L) {
   const char *dn = lua_tostring(L, 1);
   if (dn == 0) return 0;
 
@@ -82,7 +64,7 @@ static int ncm_fs_space(lua_State* L) {
   return 1;
 }
 
-static int ncm_fs_hash_value(lua_State* L) {
+int ncm_fs_hash_value(lua_State* L) {
   using std::filesystem::hash_value;
   using std::filesystem::path;
 
@@ -96,33 +78,13 @@ static int ncm_fs_hash_value(lua_State* L) {
   return 1;
 }
 
-static int ncm_int_to_hex64(lua_State* L) {
+int ncm_int_to_hex64(lua_State* L) {
   lua_Integer n = lua_tointeger(L, 1);
-#if 0
-#if 0
-  const char* digits = "0123456789abcdef";
-  std::string s("0123456789abcdef");
-
-  for (int i = 0; i < 16; ++i) {
-    lua_Integer q = n / 16;
-    lua_Integer r = n % 16;
-    s[15-i] = digits[r];
-    n = q;
-  }
-
-  lua_pushstring(L, s.c_str());
-#endif
-
-  std::ostringstream os;
-  os << "0x" << std::setbase(16) << std::setw(16) << std::setfill('0') << n << std::flush;
-  lua_pushstring(L, os.str().c_str());
-#endif
-
   lua_pushstring(L, int_to_hex64(n).c_str());
   return 1;
 }
 
-static int ncm_create_file(lua_State* L) {
+int ncm_create_file(lua_State* L) {
   char buf[4096];
 
   const char* block_fn = lua_tostring(L, 1);
@@ -148,8 +110,7 @@ static int ncm_create_file(lua_State* L) {
   return 1;
 }
 
-static const struct luaL_Reg regns [] = {
-  {"ncm_add1", ncm_add1},
+const struct luaL_Reg regns [] = {
   {"fs_space", ncm_fs_space},
   {"fs_hash_value", ncm_fs_hash_value},
   {"int_to_hex64", ncm_int_to_hex64},
@@ -157,16 +118,10 @@ static const struct luaL_Reg regns [] = {
   {NULL, NULL}  /* sentinel */
 };
 
+} // namespace wfslua
+
 extern "C" int luaopen_ncmcpp (lua_State *L) {
   lua_newtable(L);
-  luaL_setfuncs(L, regns, 0);
+  luaL_setfuncs(L, wfslua::regns, 0);
   return 1;
 }
-
-#if 0
-int main()
-{
-    const auto dirs = { "/dev/null", "/tmp", "/home", "/null" };
-    print_space_info(dirs);
-}
-#endif
